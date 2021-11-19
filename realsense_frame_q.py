b01381120844872aa210a4aed140e4cde222001b
awmckenzie <amckenzi@andrew.cmu.edu>
2021-11-19 15:41:29 -0500

    filtering stack and rpi4 compatibility

--- realsense_frame_q.py
+++ realsense_frame_q.py
@@ -1,4 +1,4 @@
-import pyrealsense2 as rs
+import pyrealsense2.pyrealsense2 as rs
 import numpy as np
 import cv2
 import time as t
@@ -28,6 +28,9 @@ try:
     hole_filter = rs.hole_filling_filter()
     spatial_filter = rs.spatial_filter(0.5, 20, 5, 0)
     temporal_filter = rs.temporal_filter()
+    depth2disparity = rs.disparity_transform()
+    disparity2depth = rs.disparity_transform(False)
+    
 
     pipeline.start(config, frame_queue)
     #pipeline.start(config)
@@ -47,17 +50,21 @@ try:
         depth_frame_filtered = depth_frame
 
         depth_frame_filtered = decimation_filter.process(depth_frame_filtered)
+        depth_frame_filtered = depth2disparity.process(depth_frame_filtered)
         depth_frame_filtered = spatial_filter.process(depth_frame_filtered)
         depth_frame_filtered = temporal_filter.process(depth_frame_filtered)
+        depth_frame_filtered = disparity2depth.process(depth_frame_filtered)
+#        depth_frame_filtered = hole_filter.process(depth_frame_filtered)
 
         t3 = t.time() # time to filter
         depth_image = np.asanyarray(depth_frame_filtered.get_data())
         for i in range(i_iter):
             for j in range(j_iter):
+                  ind = (i,j)
                 # if type(depth_image[i,j]) is not np.uint16:
                 #     depth_image[i,j] = 0
-                if depth_image[i,j] > max_dist or depth_image[i,j] < min_dist:
-                    depth_image[i,j] = 0
+#                if depth_image[i,j] > max_dist or depth_image[i,j] < min_dist:
+#                    depth_image[i,j] = 0
         t4 = t.time() # loop time
 
         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
