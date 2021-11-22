2c94a521dad865355a96be1df761cd5b0788742c
awmckenzie <amckenzi@andrew.cmu.edu>
2021-11-22 15:33:35 -0500

    Update servo.py

--- servo.py
+++ servo.py
@@ -17,19 +17,14 @@ class Servo:
     def move(self, angle):
         if self.state == 0:
             self.state = 1
-            self.target_angle = angle
-            
-            if self.target_angle > self.angle:
-                kit.continuous_servo[self.pinout].throttle = 1
-            else:
-                kit.continuous_servo[self.pinout].throttle = -1
+            self.target_angle = angle # accept new target angle    
 
         else:
             self.angle = kit.servo[self.pinout].angle
-            if self.angle == self.target_angle:
-                kit.continuous_servo[self.pinout].throttle = 0
+            if self.target_angle == self.angle:
                 self.state = 0
-
+        
+        kit.servo[self.pinout].angle = self.target_angle
 
     def get_angle(self):
         return kit.servo[self.pinout].angle
