2a863355a9d9de87ccf6e3762b3c36ba6bdb2e77
awmckenzie <amckenzi@andrew.cmu.edu>
2021-11-23 00:51:07 -0500

    Update main.py

--- realsense_servo/main.py
+++ realsense_servo/main.py
@@ -71,10 +71,10 @@ def main():
             for i in range(cfg['actuators']):
                 servos[i].move(servo_targets[i])
             
-            # depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-            # cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-            # cv2.imshow('RealSense', depth_colormap)
-            # cv2.waitKey(1)
+            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+            cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+            cv2.imshow('RealSense', depth_colormap)
+            cv2.waitKey(1)
 
     finally:
         pipeline.stop()
