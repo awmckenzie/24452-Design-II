3a9daacaf135aad975b9fd3413080169a2a98b16
awmckenzie <amckenzi@andrew.cmu.edu>
2021-11-23 15:28:01 -0500

    Update config.py

--- realsense_servo/config.py
+++ realsense_servo/config.py
@@ -1,15 +1,16 @@
 def config():
     config = {
-        'actuators':        8,
-        'x_res':            1280,
-        'y_res':            720,
-        'fps':              30,
+        'actuators':                8,
+        'x_res':                    1280,
+        'y_res':                    720,
+        'fps':                      30,
 
-        'queue_size':       1,
-        'decimation_level': 6,
-        'min_count':        50,
+        'queue_size':               1,
+        'depth_decimation_level':   6,
+        'cv_decimation_level':      2,
+        'min_count':                50,
 
-        'min_dist':         600,
-        'max_dist':         2000
+        'min_dist':                 600,
+        'max_dist':                 2000
     }
     return config
