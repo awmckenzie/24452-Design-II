305cd6f7ecf432b8f9724d128f4ec20d2cbd7941
sharonch1s <50345557+sharonch1s@users.noreply.github.com>
2021-12-01 09:50:17 -0500

    Update test.py

--- realsense_servo/test.py
+++ realsense_servo/test.py
@@ -1,7 +1,13 @@
 import numpy as np
+kit = ServoKit(channels=16)
+from adafruit_servokit import ServoKit
 
-a = np.array([[1,2,3,4,5,6], [7,8,9,10,11,12]])
+# set angle to zero
+kit.servo[6].angle = 0
+kit.servo[7].angle = 0
 
-filter = np.hsplit(a, 2)
+# a = np.array([[1,2,3,4,5,6], [7,8,9,10,11,12]])
 
-print(filter)
+# filter = np.hsplit(a, 2)
+
+# print(filter)
