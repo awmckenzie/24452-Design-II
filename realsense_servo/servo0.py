6ce9ecdf807fde74d69c244397658b7491c47800
sharonch1s <50345557+sharonch1s@users.noreply.github.com>
2021-12-01 13:42:23 -0500

    setting buttons to 0 position

---
+++ realsense_servo/servo0.py
@@ -0,0 +1,8 @@
+from adafruit_servokit import ServoKit
+kit = ServoKit(channels=16)
+
+# set angle to zero
+for i in [2,3,6,7]:
+  kit.servo[i].angle = 0
+for i in [0,1,4,5]:
+  kit.servo[i].angle = 90
