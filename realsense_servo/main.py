e315f5a76c3b61aab23d5f4e7327f94e6d8de615
sharonch1s <50345557+sharonch1s@users.noreply.github.com>
2021-11-30 22:22:33 -0500

    Update main.py

--- realsense_servo/main.py
+++ realsense_servo/main.py
@@ -8,16 +8,19 @@ import config
 
 def main():
     try:
-        cfg = config.config() # init configuration
+        cfg = config.config() # init configuration; all constants are stored in config.py
+
         ###########################################
-        
+        # servo initiation
         kit = ServoKit(channels=16)
         servos = []
         #servo_targets = [0, 0, 0, 0, 0, 0, 0, 0] # 0 to 180 degrees
         #depths = [0, 0, 0, 0, 0, 0, 0, 0] # 600 to 2000 mm
         
         servos = servo.init_servos(servos, cfg['actuators'])
+
         ###########################################
+        # intel realsense initiation
         pipeline = rs.pipeline()
 
         rs_config = rs.config()
@@ -37,6 +40,11 @@ def main():
 
         #############################################
 
+        # servos 7 6 5 4 = left of camera = right of depth map on screen
+        # servos 3 2 1 0 = right of camera = left of depth map on screen
+        # servos 7 6 opposite orientation
+        # servos 5 4 normal orientation
+
         while(True):
             frame = frame_queue.wait_for_frame()
             depth_frame = frame.as_frameset().get_depth_frame()
@@ -55,16 +63,25 @@ def main():
             servo_targets = np.zeros(cfg['actuators'])
             counts = np.zeros(cfg['actuators'])
 
+            ##### split depth map into 8 cols
             depth_image_split = np.hsplit(depth_image, cfg['actuators'])
+
             for i in range(cfg['actuators']):
+            	# if any of the column's depths (> max dist) or (< min dist), change value to zero
                 depth_filtered = np.where((depth_image_split[i] < cfg['max_dist']) & (depth_image_split[i] > cfg['min_dist']), depth_image_split[i], 0)
+                
+       			# if column has nonzero depth, take avg of col and return
                 counts[i] = np.count_nonzero(depth_filtered)
                 if counts[i] > 0:
                     depths[i] = np.mean(depth_filtered[depth_filtered != 0])
 
+            ##### calculate servo angle
             for i in range(cfg['actuators']):
-                if counts[i] > cfg['min_count']:
+            	if counts[i] > cfg['min_count']:
                     servo_targets[i] = round(90 * (depths[i] - cfg['min_dist']) / (cfg['max_dist'] - cfg['min_dist']))
+                if i in {7,6,3,2}:
+                	servo_targets[i] = -servo_targets[i] + 90
+
             print(servo_targets)
 
             for i in range(cfg['actuators']):
