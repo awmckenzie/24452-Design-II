873d6ace617105af45b4e19f8e0524195e78f7b5
awmckenzie <amckenzi@andrew.cmu.edu>
2021-12-03 14:36:58 -0500

    Update servo90degree.py

--- realsense_servo/servo90degree.py
+++ realsense_servo/servo90degree.py
@@ -1,9 +1,8 @@
 from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 
-angle = 45
-# set angle to 0
-kit.servo[0].angle = angle
-kit.servo[1].angle = angle
-kit.servo[2].angle = 90 - angle
-kit.servo[3].angle = 90 - angle
+# set angle to zero
+for i in [2,3,6,7]:
+  kit.servo[i].angle = 90
+for i in [0,1,4,5]:
+  kit.servo[i].angle = 0
