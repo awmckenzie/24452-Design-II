2d671a2e0c6d5c815ee434d65b0c396e863cceda
awmckenzie <amckenzi@andrew.cmu.edu>
2021-12-03 15:47:46 -0500

    Update servo_util.py

--- realsense_servo/servo_util.py
+++ realsense_servo/servo_util.py
@@ -2,10 +2,11 @@ import ipdb
 from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 
-ipdb.set_trace()
-def set(angle = 0, pinout = -1):
+def set(angle=0, pinout=-1):
     if pinout == -1:
         for i in range(16):
             kit.servo[i].angle = angle
     else:
         kit.servo[pinout].angle = angle
+
+ipdb.set_trace()
