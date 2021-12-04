3413c3b50f9de48525c8ff9f09007ffc3cf62d7b
awmckenzie <amckenzi@andrew.cmu.edu>
2021-12-03 19:13:16 -0500

    Update servo.py

--- realsense_servo/servo.py
+++ realsense_servo/servo.py
@@ -17,17 +17,17 @@ class Servo:
         self.min_angle = min_angle
         self.max_angle = max_angle
 
-        self.zero = 90 # angle for the central position
+        self.zero_point = 90 # angle for the central position
 
     def zero(self):
-        kit.servo[self.pinout].angle = self.zero
-        self.angle = self.zero
+        kit.servo[self.pinout].angle = self.zero_point
+        self.angle = self.zero_point
 
     def move(self, target_angle):
         if self.mirrored:
                 target_angle = -target_angle
         if abs(self.angle - target_angle) > 0.005:
-            self.target_angle = self.zero + target_angle
+            self.target_angle = self.zero_point + target_angle
             print(self.target_angle)
             kit.servo[self.pinout].angle = self.target_angle
             self.angle = kit.servo[self.pinout].angle
