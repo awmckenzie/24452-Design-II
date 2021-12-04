518598f418cdf2a5ab92c4d6a1de40c2a03bacb6
awmckenzie <amckenzi@andrew.cmu.edu>
2021-12-03 20:00:38 -0500

    Update servo.py

--- realsense_servo/servo.py
+++ realsense_servo/servo.py
@@ -29,7 +29,7 @@ class Servo:
         if abs(self.angle - target_angle) > 0.05:
             self.target_angle = self.zero_point + target_angle
             kit.servo[self.pinout].angle = self.target_angle
-            self.angle = kit.servo[self.pinout].angle
+        self.angle = kit.servo[self.pinout].angle
 
     def update_angle(self):
         self.angle = kit.servo[self.pinout].angle
