79d1e5a616d186331dc150f3d833d99ed08e3174
awmckenzie <amckenzi@andrew.cmu.edu>
2021-12-04 21:17:44 -0500

    Update main.py

--- realsense_servo/main.py
+++ realsense_servo/main.py
@@ -90,7 +90,7 @@ def main():
             ##### calculate servo angle
             for i in range(cfg['actuators']):
             	if counts[i] > cfg['min_count']:
-                    servo_targets[i] = servos[i].min_angle + round((servos[i].max_angle - servos[i].min_angle) * (cfg['max_dist'] - depths[i]) / (cfg['max_dist'] - cfg['min_dist']))
+                    servo_targets[i] = servos[i].min_angle + round((servos[i].max_angle - servos[i].min_angle) * (depths[i] - cfg['min_dist']) / (cfg['max_dist'] - cfg['min_dist']))
             #ipdb.set_trace()
             for i in range(cfg['actuators']):
                 servos[i].move(servo_targets[i])
